46ce7a2fff2bc879a18de9b856d35348317b6d66
Eric Martindale <eric@ericmartindale.com>
2024-01-18 01:58:04 -0600

    Snapshot environment

--- tests/hubs.js
+++ tests/hubs.js
@@ -4,20 +4,36 @@
 const assert = require('assert');
 const fetch = require('cross-fetch');
 const Fabric = require('@fabric/core');
+const Hub = require('@fabric/hub');
 
 // Fabric Hubs
 const HUBS = [
-  'https://hub.fabric.pub'
+  'https://hub.fabric.pub',
+  'https://labs.fabric.pub'
 ];
 
 // Tests
-describe('Fabric Hubs', function () {
-  describe('Hub', function () {
+describe('META', function () {
+  describe('Fabric', function () {
     it('is a function', function () {
       assert.strictEqual(Fabric instanceof Function, true);
     });
   });
 
+  describe('Hub', function () {
+    it('is a function', function () {
+      assert.strictEqual(Hub instanceof Function, true);
+    });
+  });
+
+  describe('Sensemaker', function () {
+    xit('is a function', function () {
+      assert.strictEqual(Sensemaker instanceof Function, true);
+    });
+  });
+});
+
+describe('Fabric Hubs', function () {
   describe('hub.fabric.pub', function () {
     it('is accessible', async function () {
       const result = await fetch(`https://hub.fabric.pub/`);
